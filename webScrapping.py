a6972cf00d0a8022e891a662e0b0b3453161b783
Arthur JEANNE <arthur.jeanne@telecomnancy.eu>
2025-02-24 15:51:33 +0100

    the folowing changement of webscrapping

--- webScrapping.py
+++ webScrapping.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 
-def function_CopyPast(url):
+def function_CopyPast(url, svg_file_name="webScrapping_svg.txt"):
 
     # Configuration de Firefox
     options = Options()
@@ -17,20 +17,23 @@ def function_CopyPast(url):
         # Récupération du contenu de la page
         page_content = driver.page_source
         # Écriture dans le fichier
-        with open("webScrapping_svg.txt", "w", encoding="utf-8") as file:
+        with open(svg_file_name, "w", encoding="utf-8") as file:
             file.write(page_content)
             
     finally:
         driver.quit()
 
-def manage_drugbank_information(file_name="webScrapping_svg.txt"):
-    with open("webScrapping_svg.txt", "r", encoding="utf-8") as file:
+def manage_drugbank_information(svg_file_name="webScrapping_svg.txt"):
+    with open(svg_file_name, "r", encoding="utf-8") as file:
         texte = file.read(page_content)
     
-    
+    texte = texte[25:417]
 
 
-file = open("webScrapping_svg.txt", "w", encoding="utf-8")
+# Utilisation :
+svg_file_name = "webScrapping_svg.txt"
+file = open(svg_file_name, "w", encoding="utf-8")
 for line in file.readlines:
     url = line[:-1]
-    function_CopyPast(url)
+    function_CopyPast(url, svg_file_name)
+    manage_drugbank_information(svg_file_name)
