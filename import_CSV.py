78d690d53a9a1a30168ffb0f47bed2d53a45138f
AUTEF Lucas <lucas.autef@telecomnancy.eu>
2025-02-25 10:59:34 +0100

    Fonction get_values_for_rows

--- import_CSV.py
+++ import_CSV.py
@@ -20,18 +20,27 @@ list_field = [
 
 df = pd.read_csv("uniprot.csv")
 
-def generate_data(): #Renvoie un dictionnaire ou les clés 
+def extract_values(): #Renvoie un dictionnaire ou les clés 
     #sont les élements de lists_field et les valeurs les listes sans redondance 
     #contenant les valeurs possibles de l'attribut
     data={column:df[column].unique().tolist() for column in list_field}
     return data
 
-def get_data(data,field): #Renvoie la liste des valeurs pour un attribut donné
+def get_attribute_values(data,field): #Renvoie la liste des valeurs pour un attribut donné
     return data.get(field)
 
-def filter(field,field_value): #Renvoie la liste des indexs de lignes de df pour chaque 
+def filter(fields,field_values): #Renvoie la liste des indexs de lignes de df pour chaque 
     #ligne qui matche field==field_value
-
     #La première ligne contenant des données correspont à l'index 0
-    filtered_df = df.loc[df[field] == field_value]
+    request=True
+    for field,value in zip(fields, field_values):
+        contition &= (df[field]==value)
+    filtered_df =df.loc(request)
     return filtered_df.index.tolist()
+
+def get_values_for_rows(list_index, list_fields):#Renvoie un dictionnaire où les clés sont les attributs de list_fields
+    #et où les valeurs sont des listes où chaque élément correspond à la valeur de l'attribut pour une ligne 
+    dic = {}
+    for field in list_fields:
+        dic[field] = df.loc[list_index,field].tolist()
+    return dic
